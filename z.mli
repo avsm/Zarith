b5e25eba0e28212f1e164d02b20042ea3f4dc868
Anil Madhavapeddy <anil@recoil.org>
2025-09-18 08:21:48 +0100

    port zarith.1.12+ox patches to zarith.1.14+ox
    from:
    https://github.com/oxcaml/opam-repository/tree/main/packages/zarith/zarith.1.12%2Box/files

--- z.mli
+++ z.mli
@@ -1,3 +1,5 @@
+@@ portable
+
 (**
    Integers.
 
@@ -46,7 +48,7 @@
 
 (** {1 Types} *)
 
-type t
+type t : immutable_data
 (** Type of integers of arbitrary length. *)
 
 exception Overflow
@@ -124,7 +126,7 @@ val of_string_base: int -> string -> t
  *)
 
 external of_substring_base
-  : int -> string -> pos:int -> len:int -> t
+  : int -> local_ string -> pos:int -> len:int -> t @@ portable
   = "ml_z_of_substring_base"
 (** [of_substring_base base s ~pos ~len] is the same as [of_string_base
     base (String.sub s pos len)]
@@ -235,27 +237,27 @@ external congruent: t -> t -> t -> bool = "ml_z_congruent"
 
 val logand: t -> t -> t
 (** Bitwise logical and. *)
-    
+
 val logor: t -> t -> t
 (** Bitwise logical or. *)
-    
+
 val logxor: t -> t -> t
 (** Bitwise logical exclusive or. *)
 
 val lognot: t -> t
-(** Bitwise logical negation. 
+(** Bitwise logical negation.
     The identity [lognot a]=[-a-1] always hold.
  *)
 
 val shift_left: t -> int -> t
-(** Shifts to the left. 
+(** Shifts to the left.
     Equivalent to a multiplication by a power of 2.
     The second argument must be nonnegative.
  *)
 
 val shift_right: t -> int -> t
-(** Shifts to the right. 
-    This is an arithmetic shift, 
+(** Shifts to the right.
+    This is an arithmetic shift,
     equivalent to a division by a power of 2 with rounding towards -oo.
     The second argument must be nonnegative.
  *)
@@ -442,7 +444,7 @@ val pp_print: Format.formatter -> t -> unit
 
 (** {1 Ordering} *)
 
-external compare: t -> t -> int = "ml_z_compare" [@@noalloc]
+external compare: t -> t -> int @@ portable = "ml_z_compare" [@@noalloc]
 (** Comparison.  [compare x y] returns 0 if [x] equals [y],
     -1 if [x] is smaller than [y], and 1 if [x] is greater than [y].
 
@@ -450,7 +452,7 @@ external compare: t -> t -> int = "ml_z_compare" [@@noalloc]
     only on OCaml 3.12.1 and later versions.
  *)
 
-external equal: t -> t -> bool = "ml_z_equal" [@@noalloc]
+external equal: t -> t -> bool @@ portable = "ml_z_equal" [@@noalloc]
 (** Equality test. *)
 
 val leq: t -> t -> bool
@@ -579,7 +581,7 @@ external kronecker: t -> t -> int = "ml_z_kronecker"
 
 external remove: t -> t -> t * int = "ml_z_remove"
 (** [remove a b] returns [a] after removing all the occurences of the
-    factor [b]. 
+    factor [b].
     Also returns how many occurrences were removed.
     @since 1.10 *)
 
@@ -681,7 +683,7 @@ val log2up: t -> int
 external size: t -> int = "ml_z_size" [@@noalloc]
 (** Returns the number of machine words used to represent the number. *)
 
-val extract: t -> int -> int -> t
+val extract: t -> int -> int -> t @@ portable
 (** [extract a off len] returns a nonnegative number corresponding to bits
     [off] to [off]+[len]-1 of [a].
     Negative [a] are considered in infinite-length 2's complement
